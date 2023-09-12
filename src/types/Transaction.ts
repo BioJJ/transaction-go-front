83558e2edf0188b28bc0822e7b9b48e02b73048c
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-12 03:30:35 -0400

    refactor: ♻️ clean code

---
+++ src/types/Transaction.ts
@@ -0,0 +1,15 @@
+import { TransactionType } from './TransactionType'
+
+export interface Transaction {
+	id: number
+
+	data: Date
+
+	description: string
+
+	value: number
+
+	seller: string
+
+	transactionType: TransactionType
+}
