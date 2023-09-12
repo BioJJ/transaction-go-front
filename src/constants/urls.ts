fdad9147d1c3910a9ac7021d2e605b32da56cec1
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-09-12 03:00:09 -0400

    feat: ✨ add constats

---
+++ src/constants/urls.ts
@@ -0,0 +1,8 @@
+export const URL_AUTH = 'http://localhost:8081/login'
+export const URL_USER_CREATE = 'http://localhost:8081/users'
+
+export const URL_USER_ID = 'http://localhost:8081/user/{userId}'
+export const URL_USER_ALL = 'http://localhost:8081/user/all'
+
+export const URL_TRANSACTION = 'http://localhost:8081/transactions'
+export const URL_TRANSACTION_CREATE = 'http://localhost:8081/transactions'
